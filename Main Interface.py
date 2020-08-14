3ecdf7e12332f94c59f5c9cd186baaebe1a1e1b5
zahoov <xavierbiancardi@gmail.com>
2020-08-13 17:29:30 -0700

    voila

--- Main Interface.py
+++ Main Interface.py
@@ -836,12 +836,11 @@ class FuelGaugeApp(App):
     def build(self):
         return MyScreenManager()
 
-    def message_setup(dt):
-        app = App.get_running_app()
+    def message_setup(self, dt):
 
         # Creates the CAN message --> arbitration_id = destination address
         try:
-            app.toggle_msg = can.Message(arbitration_id=0xCFF41F2, data=app.msg_data)
+            self.toggle_msg = can.Message(arbitration_id=0xCFF41F2, data=self.msg_data)
         except AttributeError:
             bus_status = 0
         else:
@@ -850,7 +849,7 @@ class FuelGaugeApp(App):
 
         while bus_status == 0:
             try:
-                app.toggle_msg = can.Message(arbitration_id=0xCFF41F2, data=app.msg_data)
+                self.toggle_msg = can.Message(arbitration_id=0xCFF41F2, data=self.msg_data)
             except AttributeError:
                 bus_status = 0
             else:
@@ -859,12 +858,13 @@ class FuelGaugeApp(App):
 
         # Sends the CAN message to whatever 'bus' was set to every period in seconds (0.25 = 250ms)
         try:
-            app.task = app.bus.send_periodic(app.toggle_msg, 0.25)
-            if not isinstance(app.task, can.ModifiableCyclicTaskABC):
+            self.task = self.bus.send_periodic(self.toggle_msg, 0.25)
+            if not isinstance(self.task, can.ModifiableCyclicTaskABC):
                 print("This interface doesn't seem to support modification")
-                app.task.stop()
+                self.task.stop()
                 return
         except AttributeError:
+            print('aw shit here we go again')
             pass
 
     Clock.schedule_once(message_setup)
