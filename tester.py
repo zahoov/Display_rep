06c7fb16282f363efa1cf453e124d4ee5e5cd157
zahoov <xavierbiancardi@gmail.com>
2020-09-18 16:36:35 -0700

    voila

--- tester.py
+++ tester.py
@@ -20,6 +20,9 @@ def main():
     # Continually recieved messages
     readwriteMessageThread(bus0)
 
+    print('inside main after all of the commands but before the try thing')
+
+
     # Continually write readMessages
     try:
         while True:
@@ -35,6 +38,7 @@ def setCANbaudRate(bRate):
     Make CAN interface to 250 or 500 kbps
     """
     os.system("sudo /sbin/ip link set can0 up type can bitrate " + str(bRate))
+    print('inside the baud rate thing that sets up the can network board')
 
 
 def connectToLogger(canV):
@@ -44,6 +48,7 @@ def connectToLogger(canV):
 
     try:
         bus = can.interface.Bus(channel=canV, bustype='socketcan_native')
+        print('successfully set up the actual bus')
     except OSError:
         print('Cannot find PiCAN board.')
         exit()
@@ -57,6 +62,7 @@ def readwriteMessageThread(bus):
     # Start receive thread
     t = Thread(target=can_rx_task, args=bus)
     t.start()
+    print('the new thread has been started with the bus set up')
 
 
 def createLogLine(message):
@@ -70,16 +76,20 @@ def createLogLine(message):
         fin.close()
         print(message)
 
+
     else:
         fin = open(display_code_dir + "J1587_log.txt", "w")
         fin.write(message)
         fin.close()
         print(message)
 
+    print('this is after the message is saved to a file')
+
 
 def can_rx_task(bus):
     while True:
         createLogLine(bus.recv())
+        print('this tells the other function to write to file')
 
 
 if __name__ == '__main__':
