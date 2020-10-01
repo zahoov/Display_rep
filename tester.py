3e4bbdfa5b8c22aefe3730ef5d462c9ba3432fcd
zahoov <xavierbiancardi@gmail.com>
2020-10-01 10:48:51 -0700

    voila

--- tester.py
+++ tester.py
@@ -1,16 +1,17 @@
 import serial
-from time import sleep
 
 
 def main():
     code_dir = 'Display_rep/'
     ser = serial.Serial('/dev/ttyS0')
     fin = open(code_dir + "fuel_file.txt", "w")
-    x = 1
 
-    while x == 1:
+    while True:
+
+        print('its in the loop now')
 
         try:
+            print('its the try')
             recieved_data = ser.readline()
             fin.write(str(recieved_data))
             print('Data: ' + recieved_data)
@@ -23,3 +24,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
