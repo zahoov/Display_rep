594f611563a8ec1ed3df4bf1948e59f46dc8be78
zahoov <xavierbiancardi@gmail.com>
2020-10-01 17:17:54 -0700

    voila

--- tester.py
+++ tester.py
@@ -4,27 +4,27 @@ from time import sleep
 
 def main():
     code_dir = 'Display_rep/'
-    #ser = serial.Serial('/dev/ttyS0', 9600, )
+    ser = serial.Serial('/dev/ttyS0', 9600, )
 
-    ser = serial.Serial(
+    '''ser = serial.Serial(
         port='/dev/ttyS0',
         baudrate=9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
-    )
+    )'''
 
 
 
 
-    fin = open(code_dir + "serial_data.txt", "w")
+    #fin = open(code_dir + "serial_data.txt", "w")
 
     while True:
         recieved_data = ser.read()
-        #sleep(0.03)
-        #data_left = ser.inWaiting()
-        #recieved_data += ser.read(data_left)
+        sleep(0.03)
+        data_left = ser.inWaiting()
+        recieved_data += ser.read(data_left)
         print(recieved_data)
         #fin.write(str(recieved_data))
         #print('Data: ' + recieved_data)
