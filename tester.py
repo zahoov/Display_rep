b0b995dd4833ca582a8473299e024e7ba7e68a09
zahoov <xavierbiancardi@gmail.com>
2020-10-07 17:27:24 -0700

    voila

--- tester.py
+++ tester.py
@@ -1,7 +1,15 @@
 
 
-x = b'\x80T\x00\xbe\x00\x00U\x00[\x00F\x80\xf8'
+'''x = b'\x80T\x00\xbe\x00\x00U\x00[\x00F\x80\xf8'
 
 hexlist = ['{:X}'.format(num) for num in x]
-print(*hexlist)
+print(*hexlist)'''
+
+
+wow = "80 B7 0 0 B8 0 0 33 0 DE"
+
+
+
+
+
 
