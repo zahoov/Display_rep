d51be2695fbda8948b66a89be70e65db3ea12509
zahoov <xavierbiancardi@gmail.com>
2020-10-09 10:17:44 -0700

    voila

--- tester.py
+++ tester.py
@@ -11,13 +11,17 @@ for hex in hexlist:
 print(intlist)'''
 x = [b'\x80', b'\xb7', b'\x00', b'\x00', b'\xb8', b'\x00', b'\x00', b'3', b'\x00', b'\xde']
 int_list = []
+hex_list = []
 
 for num in x:
     int_list.append(int.from_bytes(num, 'big'))
 
+for num in int_list:
+    hex_list.append(str(hex(num))[2:])
 
 
-print(int_list)
+
+print(*hex_list)
 
 #hexlist = ['{:X}'.format(num) for num in x]
 
