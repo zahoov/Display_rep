bc4e0139e3dbc2387134df8d058c940303a45402
zahoov <xavierbiancardi@gmail.com>
2020-10-29 12:19:27 -0700

    voila

--- PI_PI_TEST.py
+++ PI_PI_TEST.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     message_3 = [0xac, 0x00, 0x2e]
     '''
 
-    path = 'Display_rep/logs/' + 'ROUND3'
+    path = 'Display_rep/logs/' + 'ROUND4'
     os.system("mkdir " + path)
 
     messages = []
