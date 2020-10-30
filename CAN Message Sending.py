dfc1681825ffb1ca0bd257af282ac17249f5ce0c
zahoov <xavierbiancardi@gmail.com>
2020-10-29 17:44:11 -0700

    voila

--- CAN Message Sending.py
+++ CAN Message Sending.py
@@ -65,7 +65,7 @@ def createLogLine(message):
 def main():
     outDir = open("Display_rep/CAN2_TEST.txt", 'w')
     numCAN = 1
-    bRate = 125000
+    bRate = 250000
 
     os.system("sudo /sbin/ip link set can0 down")
     if numCAN == 2:
