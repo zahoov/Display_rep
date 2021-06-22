37687c65c96bd9ab11b11449b4836a136af3aff5
Xavier <xavierbiancardi@gmail.com>
2021-06-22 15:22:59 -0700

    nice

---
+++ run_recordCANlogger.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+numCAN=1
+bRate=500000
+CANtype="RBP19"
+truckName="bigWhite"
+
+numTank=5
+volumeStr="202,202,202,202,148"
+
+codeDir="/home/pi/recordCAN/"
+outDir="/home/pi/outCAN/${truckName}"
+
+mkdir -p /home/pi/outCAN/
+
+sudo python3 ${codeDir}recordCANlogger.py ${outDir} ${numCAN} ${bRate} ${CANtype} ${numTank} ${volumeStr}
+
